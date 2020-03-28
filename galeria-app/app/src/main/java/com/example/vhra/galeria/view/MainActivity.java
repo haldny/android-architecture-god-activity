a4b323a1c13bd0134c3fcf18feca71c463950a9d
Will Felix <willfelix93@gmail.com>
2020-03-28 14:37:03 -0300

    Merge

--- galeria-app/app/src/main/java/com/example/vhra/galeria/view/MainActivity.java
+++ galeria-app/app/src/main/java/com/example/vhra/galeria/view/MainActivity.java
@@ -1,10 +1,11 @@
-package com.example.vhra.galeria;
+package com.example.vhra.galeria.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.example.vhra.galeria.R;
 import com.example.vhra.galeria.adapter.MediasAdapter;
 import com.example.vhra.galeria.presenter.Presenter;
 import com.example.vhra.galeria.presenter.IPresenter;
